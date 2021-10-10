8f022c4e75e511ad91746a7c4343533914da563d
bonedaddy <boned4ddy@protonmail.com>
2021-10-10 15:05:18 -0700

    more ex

--- programs/event_test/src/lib.rs
+++ programs/event_test/src/lib.rs
@@ -8,12 +8,15 @@ pub mod event_test {
     pub fn initialize_a(ctx: Context<Initialize>) -> ProgramResult {
         emit!(HelloWorld{
             message: "foobar".to_string(),
-            number: 123
+            number: 123,
+            such: "baz".to_string(),
+            compute_units: "bar".to_string(),
+            very_wow: true,
         });
         Ok(())
     }
     pub fn initialize_b(ctx: Context<Initialize>) -> ProgramResult {
-        msg!("{{\"message\": \"{}\", \"number\": {}}}", "foobar", 123);
+        msg!("{{\"message\": \"{}\", \"number\": {}, \"such\": \"{}\", \"compute_units\": \"{}\", \"very_wow\": \"{}\"}}", "foobar", 123, "baz", "bar", true);
         Ok(())
     }
 }
@@ -25,6 +28,9 @@ pub struct Initialize {}
 #[event]
 pub struct HelloWorld {
     pub message: String,
-    pub number: u64
+    pub number: u64,
+    pub such: String,
+    pub compute_units: String,
+    pub very_wow: bool,
 }
 
